ebf47646629838c3544e3500257e8f5ea74e0032
Jordan Kalenitzov <69309791+Jordan-H-K@users.noreply.github.com>
2023-03-15 17:03:19 +0200

    Http response

---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/operator/map';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl : string = '../assets/album.json';
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) : Observable<any> {
+    return this._http.get(this._albumUrl)
+    .pipe(
+      map((response) =>  response.json()
+    ));
+  }
+
+}
