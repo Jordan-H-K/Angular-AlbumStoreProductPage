38f74d5513e5368da3f39d7126c93e481a0d8dda
Jordan Kalenitzov <69309791+Jordan-H-K@users.noreply.github.com>
2023-03-15 17:17:45 +0200

    update map method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
-import 'rxjs/operator/map';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,11 +9,8 @@ export class ProductService {
   private _albumUrl : string = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) : Observable<any> {
-    return this._http.get(this._albumUrl)
-    .pipe(
-      map((response) =>  response.json()
-    ));
+  getAlbum(id: Number) {
+    return this._http.get(this._albumUrl).map((response) =>  response.json());
   }
 
 }
